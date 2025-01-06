d5455741c4779562fc8c42bd7ecef9b1c3551880
aleksa <zabumaphu@gmail.com>
2025-01-05 19:47:03 -0500

    Reading fat files

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 ASM=nasm
+CC=gcc
 
 SRC_DIR=src
+TOOLS_DIR=tools
 BUILD_DIR=build
 
 .PHONY: all floppy_image kernel bootloader clean always tools_fat
@@ -17,6 +19,7 @@ $(BUILD_DIR)/main_floppy.img: bootloader kernel
 	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
 	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
 	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+	mcopy -i $(BUILD_DIR)/main_floppy.img test.txt "::test.txt"
 
 #
 # Bootloader
@@ -41,7 +44,6 @@ tools_fat: $(BUILD_DIR)/tools/fat
 $(BUILD_DIR)/tools/fat: always $(TOOLS_DIR)/fat/fat.c
 	mkdir -p $(BUILD_DIR)/tools
 	$(CC) -g -o $(BUILD_DIR)/tools/fat $(TOOLS_DIR)/fat/fat.c
-
 
 #
 # Always
--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef uint8_t bool;
 #define true 1
 #define false 0
+
 typedef struct 
 {
     uint8_t BootJumpInstruction[3];
@@ -47,12 +51,13 @@ typedef struct
     uint16_t ModifiedDate;
     uint16_t FirstClusterLow;
     uint32_t Size;
-
 } __attribute__((packed)) DirectoryEntry;
 
-BootSector g_BootSector(FILE* disk);
+
+BootSector g_BootSector;
 uint8_t* g_Fat = NULL;
 DirectoryEntry* g_RootDirectory = NULL;
+uint32_t g_RootDirectoryEnd;
 
 
 bool readBootSector(FILE* disk)
@@ -70,7 +75,7 @@ bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut)
 
 bool readFat(FILE* disk)
 {
-    g_Fat = (uint8_t*)malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
+    g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
     return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
 }
 
@@ -87,24 +92,47 @@ bool readRootDirectory(FILE* disk)
     return readSectors(disk, lba, sectors, g_RootDirectory);
 }
 
-DirectoryEntry* findFile(const char* name){
+DirectoryEntry* findFile(const char* name)
+{
     for (uint32_t i = 0; i < g_BootSector.DirEntryCount; i++)
     {
-        if (memcmp(name, g_RootDirectory[i].Name, 11) == 0){
+        if (memcmp(name, g_RootDirectory[i].Name, 11) == 0)
             return &g_RootDirectory[i];
-        }
     }
+
+    return NULL;
+}
+
+bool readFile(DirectoryEntry* fileEntry, FILE* disk, uint8_t* outputBuffer)
+{
+    bool ok = true;
+    uint16_t currentCluster = fileEntry->FirstClusterLow;
+
+    do {
+        uint32_t lba = g_RootDirectoryEnd + (currentCluster - 2) * g_BootSector.SectorsPerCluster;
+        ok = ok && readSectors(disk, lba, g_BootSector.SectorsPerCluster, outputBuffer);
+        outputBuffer += g_BootSector.SectorsPerCluster * g_BootSector.BytesPerSector;
+
+        uint32_t fatIndex = currentCluster * 3 / 2;
+        if (currentCluster % 2 == 0)
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) & 0x0FFF;
+        else
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) >> 4;
+
+    } while (ok && currentCluster < 0x0FF8);
+
+    return ok;
 }
 
 int main(int argc, char** argv)
 {
     if (argc < 3) {
-        printf("Santax: %s <disk image> <file name>\n", argv[0]);
+        printf("Syntax: %s <disk image> <file name>\n", argv[0]);
         return -1;
     }
 
     FILE* disk = fopen(argv[1], "rb");
-    if (!disk){
+    if (!disk) {
         fprintf(stderr, "Cannot open disk image %s!\n", argv[1]);
         return -1;
     }
@@ -114,13 +142,13 @@ int main(int argc, char** argv)
         return -2;
     }
 
-    if (!readFat(disk)){
+    if (!readFat(disk)) {
         fprintf(stderr, "Could not read FAT!\n");
         free(g_Fat);
         return -3;
     }
 
-    if (!readRootDirectory((disk))) {
+    if (!readRootDirectory(disk)) {
         fprintf(stderr, "Could not read FAT!\n");
         free(g_Fat);
         free(g_RootDirectory);
@@ -128,13 +156,30 @@ int main(int argc, char** argv)
     }
 
     DirectoryEntry* fileEntry = findFile(argv[2]);
-    if (!fileEntry){
+    if (!fileEntry) {
         fprintf(stderr, "Could not find file %s!\n", argv[2]);
         free(g_Fat);
         free(g_RootDirectory);
         return -5;
     }
 
+    uint8_t* buffer = (uint8_t*) malloc(fileEntry->Size + g_BootSector.BytesPerSector);
+    if (!readFile(fileEntry, disk, buffer)) {
+        fprintf(stderr, "Could not read file %s!\n", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        free(buffer);
+        return -5;
+    }
+
+    for (size_t i = 0; i < fileEntry->Size; i++)
+    {
+        if (isprint(buffer[i])) fputc(buffer[i], stdout);
+        else printf("<%02x>", buffer[i]);
+    }
+    printf("\n");
+
+    free(buffer);
     free(g_Fat);
     free(g_RootDirectory);
     return 0;
